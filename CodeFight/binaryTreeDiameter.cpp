4c6fadc24764930851abe36c13e892bb2da39574
Apoorv-R98 <32562533+Apoorv-R98@users.noreply.github.com>
2020-05-07 23:58:29 +0530

    Update binaryTreeDiameter.cpp

--- CodeFight/binaryTreeDiameter.cpp
+++ CodeFight/binaryTreeDiameter.cpp
@@ -1,1 +1,21 @@
+int height(Tree<int>*t){
+    if(t==NULL){
+        return 0;
+    }
+    return 1+max(height(t->left),height(t->right));
+}
+
+int binaryTreeDiameter(Tree<int> * t) {
+    if(t==NULL){
+        return 0;
+    }
+
+    int lheight = height(t->left);
+    int rheight = height(t->right);
+
+    int ldiameter = binaryTreeDiameter(t->left);
+    int rdiameter = binaryTreeDiameter(t->right);
+  
+    return max(lheight+rheight+1,max(ldiameter,rdiameter));
+}
 
