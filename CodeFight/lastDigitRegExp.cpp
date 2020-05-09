473e60f848aa9b24de015ae237cbf21a02a7fff0
Apoorv-R98 <32562533+Apoorv-R98@users.noreply.github.com>
2020-05-10 01:12:20 +0530

    Update and rename lastDigitRegExp to lastDigitRegExp.cpp

---
+++ CodeFight/lastDigitRegExp.cpp
@@ -0,0 +1,18 @@
+Given a string with at least one digit, extract the rightmost digit from it.
+
+Example
+
+For inputString = "var_1__Int", the output should be
+lastDigitRegExp(inputString) = '1';
+For inputString = "qq2q", the output should be
+lastDigitRegExp(inputString) = '2';
+For inputString = "0ss", the output should be
+lastDigitRegExp(inputString) = '0'.
+
+char lastDigitRegExp(std::string inputString) {
+    for(int i=inputString.size();i>=0;i--){
+        if(inputString[i]>='0' && inputString[i]<='9'){
+            return inputString[i];
+        }
+    }
+}
