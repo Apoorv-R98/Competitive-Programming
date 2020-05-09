ba493d634abc3385927018a247ef942e757f6b67
Apoorv-R98 <32562533+Apoorv-R98@users.noreply.github.com>
2020-05-10 01:10:53 +0530

    Update binaryTreeDiameter.cpp

--- CodeFight/binaryTreeDiameter.cpp
+++ CodeFight/binaryTreeDiameter.cpp
@@ -1,3 +1,9 @@
+// Let's define a tree's diameter as the number of nodes on the longest path between any two nodes in the tree. 
+// The longest path may or may not include the tree's root. Given a binary tree, find its diameter.
+// In the diagrams in the examples below, the longest path is indicated by blue nodes.
+
+
+
 int height(Tree<int>*t){
     if(t==NULL){
         return 0;
